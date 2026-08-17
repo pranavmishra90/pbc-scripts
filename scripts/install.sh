6a396098abd4bae26f2a5eb2da590f06199d9b47
Pranav Kumar Mishra <62562712+pranavmishra90@users.noreply.github.com>
2026-08-17 17:26:44 -0500

    feat: add helper information to the end of the scripts, with the remote git URL they were cloned from

--- scripts/install.sh
+++ scripts/install.sh
@@ -9,6 +9,17 @@ for script in $SCRIPTS; do
   if [ ! -f "$REPO_SCRIPTS_DIR/$script" ]; then
     echo "Error: $REPO_SCRIPTS_DIR/$script does not exist. Aborting installation."
     exit 1
+  else
+    REPO_REMOTE_URL=$(git config --get remote.origin.url)
+    cat "$REPO_SCRIPTS_DIR/$script" <<EOF
+
+# --- Script Info ---
+# This script was installed from the pbc-scripts repository.
+#
+# For more information, visit the remote repository at:
+# $REPO_REMOTE_URL
+# -------------------
+EOF
   fi
 done
 
